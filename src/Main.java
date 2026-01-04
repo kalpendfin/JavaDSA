1e10cc8251d7899d35d7f1508158a55ea0e61766
Kalpen <kalpen@dfin.ai>
2026-01-04 12:50:34 +0530

    First Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,6 @@ public class Main {
         // Press Ctrl+1 with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
-
         // Press Alt+Shift+X or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
             // Press Alt+Shift+D to start debugging your code. We have set one breakpoint
