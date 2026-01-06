ce4e8dde464b95602ccfb70caf3ca2b4ed92d1ec
Kalpen <kalpen@dfin.ai>
2026-01-06 21:19:09 +0530

    First Initial Commit

---
+++ src/assignment/basic/AreaOfEquilateralTriangle.java
@@ -0,0 +1,14 @@
+package assignment.basic;
+
+import java.util.Scanner;
+
+public class AreaOfEquilateralTriangle {
+    public static void main(String[] args) {
+        System.out.print("Enter area of Equilateral Triangle : ");
+        Scanner scanner = new Scanner(System.in);
+        int height = scanner.nextInt();
+        double area = Math.sqrt(3) * (double) (height * height) / 4;
+        System.out.println("Area of Equilateral Triangle : " + area);
+
+    }
+}
