64b03d5149a68430bfb1a28f9e4887cbca26abb5
Kalpen <kalpen@dfin.ai>
2026-01-06 16:14:34 +0530

    First Initial Commit

---
+++ src/assignment/basic/AreaOfTriangle.java
@@ -0,0 +1,16 @@
+package assignment.basic;
+
+import java.util.Scanner;
+
+public class AreaOfTriangle {
+    public static void main(String[] args) {
+        System.out.print("Enter Height of Triangle : ");
+        Scanner scanner = new Scanner(System.in);
+        int height = scanner.nextInt();
+        System.out.print("Enter Base : ");
+        int base = scanner.nextInt();
+        double area = 0.5 * height * base;
+        System.out.println("Area of Circle : " + area);
+
+    }
+}
