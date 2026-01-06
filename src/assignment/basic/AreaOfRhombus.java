dd87f455bdc7ca58f81e5537e25aab2a187e9319
Kalpen <kalpen@dfin.ai>
2026-01-06 20:23:54 +0530

    First Initial Commit

---
+++ src/assignment/basic/AreaOfRhombus.java
@@ -0,0 +1,16 @@
+package assignment.basic;
+
+import java.util.Scanner;
+
+public class AreaOfRhombus {
+    public static void main(String[] args) {
+        System.out.print("Enter p-Diagonal of Rhombus : ");
+        Scanner scanner = new Scanner(System.in);
+        int height = scanner.nextInt();
+        System.out.print("Enter q-Diagonal of Rhombus : ");
+        int base = scanner.nextInt();
+        double area = (double) (height * base) / 2;
+        System.out.println("Area of Rhombus : " + area);
+
+    }
+}
