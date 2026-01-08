35a25f2b79f7fa8072c031e36de30f593d276033
Kalpen <kalpen@dfin.ai>
2026-01-08 10:44:55 +0530

    First Initial Commit

--- src/assignment/basic/PerimeterOfEquilateralTriangle.java
+++ src/assignment/basic/PerimeterOfEquilateralTriangle.java
@@ -1,7 +1,12 @@
 package assignment.basic;
 
+import java.util.Scanner;
+
 public class PerimeterOfEquilateralTriangle {
     public static void main(String[] args) {
-
+        System.out.print("Enter Side of Equilateral Triangle : ");
+        Scanner scanner = new Scanner(System.in);
+        int area = scanner.nextInt();
+        System.out.println("Perimeter of Equilateral Triangle : " + 3 * area);
     }
 }
