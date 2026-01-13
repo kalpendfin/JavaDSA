9898c86b61dd77b6dc11605c41f296bdd607c099
Kalpen <kalpen@dfin.ai>
2026-01-13 08:56:05 +0530

    First Initial Commit

--- src/assignment/Intermediate/CalculateCGPA.java
+++ src/assignment/Intermediate/CalculateCGPA.java
@@ -1,7 +1,17 @@
 package assignment.Intermediate;
 
+import java.util.Scanner;
+
 public class CalculateCGPA {
     public static void main(String[] args) {
+        System.out.println("Calculate CGPA");
+        System.out.print("Enter Total Marks : ");
+        Scanner scanner = new Scanner(System.in);
+        int total = scanner.nextInt();
+        System.out.print("Enter No of Subjects : ");
+        int subject = scanner.nextInt();
 
+        System.out.println("CGPA = " + (total / subject));
+        scanner.close();
     }
 }
