8f9a5a4f496640961d79c1f5b9021a6341120a92
Kalpen <kalpen@dfin.ai>
2026-01-19 09:58:51 +0530

    First Initial Commit

---
+++ src/assignment/recursion/Nto1.java
@@ -0,0 +1,27 @@
+package assignment.recursion;
+
+import java.util.Scanner;
+
+public class Nto1 {
+    public static void main(String[] args) {
+        System.out.print("Enter a Number : ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+
+//        backWard(number);
+        forward(number);
+
+    }
+
+    private static void forward(int number) {
+        if (number == 0) return;
+        forward(number - 1);
+        System.out.println(number);
+    }
+
+    private static void backWard(int number) {
+        if (number == 0) return;
+        System.out.println(number);
+        backWard(number - 1);
+    }
+}
